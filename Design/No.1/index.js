0effc80106531dd2822fa958ddd371c64f13cc16
chinti <chinti@youremail.com>
2018-04-25 12:27:40 +0800

    no.1

---
+++ Design/No.1/index.js
@@ -0,0 +1,24 @@
+﻿
+function transferColor(){
+	var divColor = document.getElementsByTagName("div");
+	var buttonColor = document.getElementsByTagName("button");
+	var pColor = document.getElementsByTagName("p");
+
+	buttonColor[0].onclick = function() {
+		if (divColor[0].getAttribute("class") == "color")
+		{
+		divColor[0].setAttribute("class","active");
+		pColor[0].setAttribute("class","active");
+		}
+		else if (divColor[0].getAttribute("class") == "active")
+		{
+		divColor[0].setAttribute("class","color");
+		pColor[0].setAttribute("class","color");
+		}
+
+	}
+}
+	
+window.onload = function() {
+	transferColor();
+}
