a3cc1e29985957be8730c603ab8d475c2d6e4019
chinti <chinti@youremail.com>
2018-04-27 00:07:43 +0800

    no.5

---
+++ Design/No.6/index.js
@@ -0,0 +1,21 @@
+﻿function showPic() {
+	var choose = document.getElementsByClassName("choBox");
+	var show = document.getElementsByClassName("show");
+	for (var i = 0 ; i< show.length ; i++) {
+		(function() {
+			var temp = i;
+			choose[temp].onclick = function() {
+				for(let j = 0 ; j < show.length ; j++) {
+					
+					show[j].setAttribute('class','show');
+				}
+				show[temp].setAttribute('class','show active'+(temp+1));
+			}
+		})();
+
+	}
+}
+
+window.onload = function() {
+	showPic();	
+};
