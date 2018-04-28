7596f19dad34c91dad5ebf3e92e6e68c4b3c3592
chinti <chinti@youremail.com>
2018-04-28 20:23:26 +0800

    no.8

---
+++ Design/No.8/scripts/index.js
@@ -0,0 +1,27 @@
+﻿ $(document).ready(function() {
+	lottie.loadAnimation({
+		container:		$(".stage")[0],
+		renderer:		'svg',
+		loop:			true,
+		autoplay:		true,
+		path:			'./assets/data1.json',
+	}); 
+	var ani = lottie.loadAnimation({
+		container:		$(".stage")[1],
+		renderer:		'svg',
+		loop:			false,
+		autoplay:		false,
+		path:			'./assets/data2.json',
+	}); 
+	
+	$(".rectangle").mouseenter(function(){
+		ani.play();
+	});
+	$(".rectangle").mouseleave(function(){
+		ani.stop();
+	});
+	
+});
+
+
+
