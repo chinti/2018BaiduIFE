0ba063bb357ea0e702c40ce621586a963056b8a7
chinti <chinti@youremail.com>
2018-04-28 11:22:52 +0800

    no.7

---
+++ Design/No.7/scripts/index.js
@@ -0,0 +1,16 @@
+﻿$(".form__input").blur(function(){
+	if ($(".form__input")[0].value && $(".form__input")[1].value) {
+		$(".btn").addClass("animated infinite pulse");
+	}
+	else {
+		$(".btn").removeClass("animated infinite pulse");
+	}
+		
+	if ($(this).val()) {
+		$(this).next().children(".form__label__content").addClass("after");
+	}
+	else {
+		$(this).next().children(".form__label__content").removeClass("after");
+	}
+	
+});
