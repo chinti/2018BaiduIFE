d5b8acf52ae7e684f9f64cb7670d129d7671ce63
chinti <chinti@youremail.com>
2018-05-06 15:02:56 +0800

    1.2new

---
+++ MVVM/1.2/src/index.js
@@ -0,0 +1,26 @@
+import san from 'san'
+import './styles/index.css'
+
+import { router } from 'san-router';
+//import Hello from './app/hello';
+
+
+
+//router.add({rule: '/', Component: Hello, target: '#app'});
+var MyApp = san.defineComponent({
+    template: '<p>Hello {{name}}!</p>',
+
+    initData: function () {
+        return {
+            name: 'San'
+        };
+    }
+});
+
+
+var myApp = new MyApp();
+myApp.attach(document.body);
+
+
+
+router.start()
